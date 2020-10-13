ee1eb187f084dfff953df4eaca1f4e5e214fba38
tumtpk <pk.prateep@gmail.com>
2020-10-13 18:29:16 +0700

    edit

--- server/src/app.js
+++ server/src/app.js
@@ -1,34 +1,35 @@
 let express = require('express')
 let bodyParser = require('body-parser')
-const {sequelize} = require('./models')
+// let cors = require('cors')
+const { sequelize } = require('./models')
 
 const config = require('./config/config')
 
 const app = express()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(cors())
 
-require('./routes') (app)
+require('./routes')(app)
 
-
-app.get('/status', function (req, res){
-    res.send('Hello NodeJs Server')
+app.get('/status', function(req, res) {
+    res.send('Hello nodejs server')
 })
 
-app.get('/hello/:person', function(req,res){
-    console.log('hello -' + req.params.person)
-    res.send('say hello with ' + req.params.name)
+app.get('/hello/:person', function(req, res) {
+    console.log('hello - ' + req.params.person)
+    res.send('sey hello with ' + req.params.person)
 })
 
-app.post('/hello', function(req, res){
-    res.send('OK you post -' + req.body.name)
+app.post('/hello', function(req, res) {
+    res.send('OK  you post - ' + req.body.name)
 })
 
 let port = process.env.PORT || config.port
 
-sequelize.sync({force: false}).then(() => {
-    app.listen(port, function () {
-       console.log('Server running on ' + port)
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, function() {
+        console.log('Server running on ' + port)
     })
 })
