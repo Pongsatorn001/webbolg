aec11e517382b81a9ad24aeb7dfd83001e64e3d4
tumtpk <pk.prateep@gmail.com>
2020-10-14 22:15:40 +0700

    Create vue

---
+++ client/src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+//Import Component
+//Users
+import UserIndex from '@/components/Users/Index'
+import UserCreate from '@/components/Users/CreateUser'
+import UserEdit from '@/components/Users/EditUser'
+import UserShow from '@/components/Users/ShowUser'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/users',
+      name: 'users',
+      component: UserIndex
+    },
+    {
+      path: '/users/create',
+      name: 'users-create',
+      component: UserCreate
+    },
+    {
+      path: '/users/edit',
+      name: 'users-edit',
+      component: UserEdit
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: UserShow
+    },
+  ]
+})
